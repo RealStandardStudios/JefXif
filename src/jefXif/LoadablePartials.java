1de181ed9d22e0d12ae485d41a61c525a8c4c919
Matthew <lololirpro@gmail.com>
2014-08-07 16:52:33 +1200

    Edited the loadable partials class to allow for the controller's reference to the interface/gui to be set properly

--- src/jefXif/LoadablePartials.java
+++ src/jefXif/LoadablePartials.java
@@ -7,12 +7,14 @@ import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 public interface LoadablePartials {
-	default Node loadPartial(String name) throws IOException {
+	default Node loadPartial(String name, Gui ui) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(this.getClass().getResource(
 				Strings.FileLoc + name + "Partial.fxml"));
 		
 		AnchorPane partial = loader.load();
+		WindowController controller = loader.getController();
+		controller.setInterface(ui);
 		return partial;
 	}
 }
