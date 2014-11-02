d28b184a18ab2fbb45242010c5cd3d19d160ce6c
Matthew <lololirpro@gmail.com>
2014-11-02 14:21:23 +1300

    fixed so it would work outside of the dev env

--- src/jefXif/interfaces/PartialLoader.java
+++ src/jefXif/interfaces/PartialLoader.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
-import jefXif.Strings;
 import jefXif.view.Gui;
 import jefXif.view.WindowController;
 
@@ -14,10 +13,9 @@ import jefXif.view.WindowController;
  * @author Real Standard Studios - Matthew Meehan
  */
 public interface PartialLoader {
-	default WindowController loadPartial(String name, Gui ui) throws IOException {
+	default WindowController loadPartial(String name, Gui ui, Class className) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(this.getClass().getResource(
-				Strings.FileLoc + name + "Partial.fxml"));
+		loader.setLocation(className.getResource(name + "Partial.fxml"));
 		
 		AnchorPane partial = loader.load();
 		WindowController controller = loader.getController();
